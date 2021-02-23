fcc3b7bfcdd3834bf216d6728d0b941ae006066a
Oleg-Vaskevich <oleg_vas08@mail.ru>
2021-02-23 14:08:21 +0300

    feat: main style

--- dist/script.js
+++ dist/script.js
@@ -86,31 +86,58 @@ function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableTo
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
 function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 
 var Board = function Board() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", {
-    className: "main"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section", {
-    className: "board"
-  }, _toConsumableArray(Array(16).keys()).map(function (elem, id) {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(["tile"]),
+      _useState2 = _slicedToArray(_useState, 2),
+      tileStyle = _useState2[0],
+      setTileStyle = _useState2[1];
+
+  var tilesArray = _toConsumableArray(Array(16).keys()).map(function (elem, id) {
     return (
       /*#__PURE__*/
       // eslint-disable-next-line react/no-array-index-key
       react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
         key: id,
-        className: "tile"
+        className: _toConsumableArray(tileStyle)
       }, "0")
     );
-  })));
+  });
+
+  var addClass = function addClass(newClass) {
+    setTileStyle([].concat(_toConsumableArray(tileStyle), [newClass]));
+  };
+
+  var deleteClass = function deleteClass(classToDelete) {
+    setTileStyle([tileStyle.filter(function (elem) {
+      return elem !== classToDelete;
+    })]);
+  };
+
+  console.log(tilesArray);
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", {
+    className: "main"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("section", {
+    className: "board"
+  }, _toConsumableArray(tilesArray)));
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Board);
--- src/modules/Board.jsx
+++ src/modules/Board.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Board = () => (
-  <main className="main">
-    <section className="board">
-      {[...Array(16).keys()].map((elem, id) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <div key={id} className="tile">
-          0
-        </div>
-      ))}
-    </section>
-  </main>
-);
+
+const Board = () => {
+  const  [tileStyle, setTileStyle] = useState(["tile"]);
+
+  const tilesArray = [...Array(16).keys()].map((elem, id) => (
+    // eslint-disable-next-line react/no-array-index-key
+    <div key={id} className={[...tileStyle]}>
+      0
+    </div>
+  ))
+
+  const addClass = (newClass) => {
+    setTileStyle([...tileStyle, newClass]);
+  }
+
+  const deleteClass = (classToDelete) => {
+    setTileStyle([tileStyle.filter((elem) => elem !== classToDelete)]);
+  }
+
+  console.log(tilesArray);
+
+  return (
+    <main className="main">
+      <section className="board">
+          {[...tilesArray]}
+      </section>
+    </main>
+  )
+
+}
+
 
 export default Board;
